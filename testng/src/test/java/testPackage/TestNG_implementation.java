de02acb66545ef0220d1b1b01a9b7f2fe743b427
Ashutosh Nile <Ashutosh_Nile@epam.com>
2018-01-29 17:01:04 +0530

    updated code

---
+++ testng/src/test/java/testPackage/TestNG_implementation.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class TestNG_implementation {
+	
+	
+	@Test
+	public void print() {
+		System.out.println("This is Test Method");
+	}
+	
+
+}
